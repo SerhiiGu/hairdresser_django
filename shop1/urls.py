fa379ccc2d3528c7e15de81bfacf18cd25183db2
Serhii Humeniuk <gumenyuk.sergey.first@gmail.com>
2023-03-18 16:51:34 +0200

    add some views

--- shop1/urls.py
+++ shop1/urls.py
@@ -1,6 +1,19 @@
-from django.contrib import admin
 from django.urls import path
+import shop1.views
+
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', shop1.views.root_handler),
+    path('services/', shop1.views.services_handler),
+    path('services/<int:service_id>/', shop1.views.service_id_handler),
+    path('specialist/', shop1.views.specialist_handler),
+    path('specialist/<specialist_id>/', shop1.views.specialist_id_handler),
+    path('booking/', shop1.views.booking_handler),
+    path('user/', shop1.views.user_page),
+    path('panel/', shop1.views.panel_page),
+    path('panel/booking/', shop1.views.panel_booking_page),
+    path('panel/specialist/', shop1.views.panel_specialist_page),
+    path('panel/specialist/<specialist_id>', shop1.views.panel_specialist_id_page),
+    path('login/', shop1.views.login_handler),
+    path('register/', shop1.views.register_handler),
 ]
--- shop1/views.py
+++ shop1/views.py
@@ -1,3 +1,54 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def root_handler(request):
+    return render(request, "index.html")
+
+
+def services_handler(request):
+    return HttpResponse("Service page!")
+
+
+def service_id_handler(request, service_id):
+    return HttpResponse("Service page of %s!" % service_id)
+
+
+def specialist_handler(request):
+    return HttpResponse("Specialist page")
+
+
+def specialist_id_handler(request, specialist_id):
+    return HttpResponse("Specialist %s page" % specialist_id)
+
+
+def booking_handler(request):
+    return HttpResponse("Booking page")
+
+
+def user_page(request):
+    return HttpResponse("User page")
+
+
+def panel_page(request):
+    return HttpResponse("User panel page")
+
+
+def panel_booking_page(request):
+    return HttpResponse("panel_booking_page")
+
+
+def panel_specialist_page(request):
+    return HttpResponse("panel_specialist_page")
+
+
+def panel_specialist_id_page(request):
+    return HttpResponse("panel_specialist_id_page")
+
+
+def login_handler(request):
+    return HttpResponse("login_handler")
+
+
+def register_handler(request):
+    return HttpResponse("register_handler")
